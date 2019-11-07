4db5719966f7b85d1b584684ff12c9d9aee3629f
Ruth Ansah-Vaughan <19594471+ruthansah@users.noreply.github.com>
2019-11-07 16:50:21 -0500

    removed youtube api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDBqiQxdZaRlbQhtwZ9I7LGRLmzcatLzYA';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
